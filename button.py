a8e72e5ca681aa5f9791bea43c74ba999cfb8035
Angelus123 <izerefaifelix@gmail.com>
2023-03-14 09:19:53 +0200

    improve button

--- button.py
+++ button.py
@@ -1,7 +1,18 @@
 from tkinter import *
+
 root = Tk()
+
+
 def myClick():
     Label(root, text="Look! I have clicked a button").pack()
-myButton = Button(root, text="click Me!", padx=50, pady=30, command =myClick).pack()
+
+
+myButton = Button(root,
+                  text="click Me!",
+                  padx=50,
+                  pady=15,
+                  command=myClick,
+                  fg="white",
+                  bg="green").pack()
 #Shoving it onto screen
 root.mainloop()
